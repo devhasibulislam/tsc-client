a3250a5050b8ae075b5b8eea6fec289a5d9e693e
hasibulislam999 <hasib143sl@gmail.com>
2022-07-09 17:28:50 +0600

    applying comments to identify nested

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Dashboard from './routes/dashboard/Dashboard';
 import Todo from './routes/dashboard/Todo';
 import ViewStudent from './routes/dashboard/ViewStudent';
 import ViewTeacher from './routes/dashboard/ViewTeacher';
+import Header from './routes/Header';
 import Home from './routes/Home';
 import Login from './routes/Login';
 import Register from './routes/Register';
@@ -13,15 +14,18 @@ import Register from './routes/Register';
 function App() {
   return (
     <section>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+        
+        {/* dashboard sidebar section */}
         <Route path='/dashboard' element={<Dashboard />}>
-          <Route index element={<AddStudent />} />
-          <Route path='viewStudent' element={<ViewStudent />} />
-          <Route path='addTeacher' element={<AddTeacher />} />
+          <Route index element={<ViewStudent />} />
+          <Route path='addStudent' element={<AddStudent />} />
           <Route path='viewTeacher' element={<ViewTeacher />} />
+          <Route path='addTeacher' element={<AddTeacher />} />
           <Route path='todo' element={<Todo />} />
         </Route>
       </Routes>

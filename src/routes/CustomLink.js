18d7fe5b4ca03c93cf2dc179dff43b9ba930991c
hasibulislam999 <hasib143sl@gmail.com>
2022-07-09 16:08:33 +0600

    clicked link to be activated

---
+++ src/routes/CustomLink.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+    Link,
+    useMatch,
+    useResolvedPath,
+} from "react-router-dom";
+
+function CustomLink({ children, to, ...props }) {
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+
+    return (
+        <div>
+            <Link
+                style={{ color: match ? "#7f0e0e" : "#3a3a3a" }}
+                to={to}
+                {...props}
+            >
+                {children}
+            </Link>
+        </div>
+    );
+}
+
+export default CustomLink;

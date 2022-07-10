3c6151e0be603333f6c194e33acd3319eccf61d4
hasibulislam999 <hasib143sl@gmail.com>
2022-07-11 00:43:15 +0600

    componentaly model and integrate with delete teacher done

---
+++ src/routes/DeleteModal.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const DeleteModal = ({ user, refetch }) => {
+    const handleDeleteUser = () => {
+        const deleteUser = async () => {
+            const request = await fetch(`http://localhost:5000/teacher/${user?._id}`, {
+                method: "DELETE"
+            });
+            const response = await request.json();
+            console.log(response);
+            refetch();
+        };
+        deleteUser();
+    };
+    return (
+        <section>
+            <input type="checkbox" id="delete-modal" class="modal-toggle" />
+            <div class="modal modal-bottom sm:modal-middle">
+                <div class="modal-box">
+                    <h3 class="font-bold text-xl">Are you sure?</h3>
+                    <p class="py-4">You are going to delete a <span className='font-semibold'>{user.role}</span> named <span className='font-semibold  border-b-2 border-b-black pb-1'>{user.name}</span></p>
+                    <div class="modal-action">
+                        <label for="delete-modal" class="btn btn-success text-white" onClick={handleDeleteUser}>Delete</label>
+                        <label for="delete-modal" class="btn btn-error text-white">Cancel</label>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default DeleteModal;

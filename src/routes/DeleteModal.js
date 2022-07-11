c324b403aba7be05442d9afe09b3469a4f77642a
hasibulislam999 <hasib143sl@gmail.com>
2022-07-11 20:50:14 +0600

    fix bugs of delete modal

--- src/routes/DeleteModal.js
+++ src/routes/DeleteModal.js
@@ -7,8 +7,8 @@ const DeleteModal = ({ user, refetch }) => {
                 method: "DELETE"
             });
             const response = await request.json();
-            console.log(response);
-            refetch();
+            if (response.acknowledged)
+                refetch();
         };
         deleteUser();
     };
@@ -18,7 +18,7 @@ const DeleteModal = ({ user, refetch }) => {
             <div class="modal modal-bottom sm:modal-middle">
                 <div class="modal-box">
                     <h3 class="font-bold text-xl">Are you sure?</h3>
-                    <p class="py-4">You are going to delete a <span className='font-semibold'>{user.role}</span> named <span className='font-semibold  border-b-2 border-b-black pb-1'>{user.name}</span></p>
+                    <p class="py-4">You are going to delete a <span className='font-semibold'>{user.role}</span> named <span className='font-semibold  border-b-2 border-b-black pb-1 text-[#861414]'>{user.name}</span></p>
                     <div class="modal-action">
                         <label for="delete-modal" class="btn btn-success text-white" onClick={handleDeleteUser}>Delete</label>
                         <label for="delete-modal" class="btn btn-error text-white">Cancel</label>
